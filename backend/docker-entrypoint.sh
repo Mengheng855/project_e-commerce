d1d6b2a37e91037693c53882d7c76d5dd0738036
Mengheng855 <mengheng.mh855@gmail.com>
2026-06-06 16:15:40 +0700

    fix telegram

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 set -e
 
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
+if [ "$1" = "php" ] && [ "$2" = "artisan" ] && [ "$3" = "serve" ]; then
+    php artisan config:cache
+    php artisan route:cache
+    php artisan view:cache
+fi
 
 exec "$@"
